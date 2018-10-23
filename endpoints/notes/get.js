bfa676c040891a4e4b81b82d487c2d7c701d7fa7
Michael Lindstrom <michaellindstr@gmail.com>
2018-10-23 09:00:54 -0400

    First commit. Backend api setup through AWS and serverless

---
+++ endpoints/notes/get.js
@@ -0,0 +1,24 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context, callback) {
+  const params = {
+    TableName: "notes",
+    Key: {
+      userID: event.requestContext.identity.cognitoIdentityId,
+      notesID: event.pathParameters.id
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      callback(null, success(result.Item));
+    } else {
+      callback(null, failure({ status: false, error: "Item not found." }));
+    }
+  } catch (e) {
+    console.log("error: ", e);
+    callback(null, failure({ status: false }));
+  }
+}
